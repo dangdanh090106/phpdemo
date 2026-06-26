217926481c233f1eea656980323f48a3d80f9d8d
dangdanh090106 <dkct915@gmail.com>
2026-06-26 09:43:15 +0700

    BaiTap1

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// 1. Khai báo các biến thông tin sinh viên
+$ho_ten = "Đặng Công Danh";
+$tuoi = 20;
+$nganh_hoc = "Công nghệ thông tin";
+$email = "dangdanh0901@gmail.com";
+?>
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Trang chào sinh viên</title>
+</head>
+<body>
+
+    <div class="card">
+        <h2>Thông Tin Sinh Viên</h2>
+        
+        <p>Họ tên: <?php echo $ho_ten; ?></p>
+        <p>Tuổi: <?php echo $tuoi; ?></p>
+        <p>Ngành học: <?php echo $nganh_hoc; ?></p>
+        <p>Email: <?php echo $email; ?></p>
+
+        <?php 
+        if ($tuoi >= 18) {
+            echo "<p><strong>Đủ tuổi học đại học</strong></p>";
+        } 
+        ?>
+    </div>
+
+</body>
+</html>
